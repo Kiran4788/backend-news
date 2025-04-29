92d58877b2f227f7ff7de3c08b143d919db24284
Kiran4788 <kiranmai.mamidala@gmail.com>
2025-04-29 15:28:52 +0000

    Add task 10

--- app/models/articles.model.js
+++ app/models/articles.model.js
@@ -12,10 +12,26 @@ exports.selectArticleById = (article_id) => {
     });
 };
 
-exports.selectAllArticles = () => {
-  return db
-    .query(
-      `SELECT a.author, 
+exports.selectAllArticles = (sort_by, order) => {
+  const validSortBy = [
+    "article_id",
+    "title",
+    "topic",
+    "author",
+    "created_at",
+    "votes",
+  ];
+  const validOrder = ["asc", "desc"];
+  if (sort_by && !validSortBy.includes(sort_by.toLowerCase())) {
+    return Promise.reject({ status: 400, msg: "Invalid sort query" });
+  }
+  if (order && !validOrder.includes(order.toLowerCase())) {
+    return Promise.reject({ status: 400, msg: "Invalid order query" });
+  }
+  const orderBy = sort_by ? ` ORDER BY ${sort_by} ` : ` ORDER BY created_at `;
+  const orderDirection = order ? order : " DESC";
+  const orderClause = ` ${orderDirection}`;
+  const query = `SELECT a.author, 
          a.title, 
          a.article_id, 
          a.topic, 
@@ -26,11 +42,10 @@ exports.selectAllArticles = () => {
          FROM articles AS a 
          LEFT JOIN comments AS c ON a.article_id = c.article_id
          GROUP BY 1,2,3,4,5,6,7
-         ORDER BY created_at DESC`
-    )
-    .then((result) => {
-      return result.rows;
-    });
+         ${orderBy}${orderClause}`;
+  return db.query(query).then((result) => {
+    return result.rows;
+  });
 };
 
 exports.updateArticleVotes = (article_id, inc_votes) => {
